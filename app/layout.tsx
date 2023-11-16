afa3a352baa6ba57ec0d5cad4a913e9b2481ad19
kimthuy168 <thuy38092@gmail.com>
2023-11-16 23:33:42 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "KimThuy | Personal Portfolio",
+  description: "KimThuy is a full-stack developer with 2 years of experience.",
 };
 
 export default function RootLayout({
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+    <head>
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+    </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
